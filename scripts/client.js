e059f07644f3163d1b053613b58ccd23ef10678f
Kurisu Brooks <kurisubrooks@gmail.com>
2016-07-17 02:14:03 +1000

    ES6 `let` instead of `var`, Moved Core to `document.ready`

--- scripts/client.js
+++ scripts/client.js
@@ -5,9 +5,9 @@ const {remote} = electron
 const {dialog} = remote
 
 const $ = require("jquery")
-const dropbox = document.getElementById("global")
 const $editor = $("#editor")
 const $title = $("#title")
+const drop = document.getElementById("global")
 
 function checkFileType(name, mime) {
     let splice = name.split(".")
@@ -41,59 +41,69 @@ function saveDocument(type) {
 }
 
 function insertAtCursor(value) {
-    var cursorPos = $editor.prop("selectionStart")
-    var v = $editor.val()
-    var textBefore = v.substring(0, cursorPos)
-    var textAfter  = v.substring(cursorPos, v.length)
+    let cursorPos = $editor.prop("selectionStart")
+    let v = $editor.val()
+    let textBefore = v.substring(0, cursorPos)
+    let textAfter  = v.substring(cursorPos, v.length)
     $editor.val(textBefore + $(this).val() + textAfter)
 }
 
 function typeInTextarea(ins) {
-    var start = $editor.prop("selectionStart")
-    var end = $editor.prop("selectionEnd")
-    var text = $editor.val()
-    var before = text.substring(0, start)
-    var after  = text.substring(end, text.length)
+    let start = $editor.prop("selectionStart")
+    let end = $editor.prop("selectionEnd")
+    let text = $editor.val()
+    let before = text.substring(0, start)
+    let after  = text.substring(end, text.length)
     $editor.val(before + ins + after)
     $editor[0].selectionStart = $editor[0].selectionEnd = start + ins.length
     $editor.focus()
     return false
 }
 
-document.onkeydown = function (e) {
-    // ctrl/cmd + s
-    if (e.ctrlKey || e.metaKey) {
-        if (e.keyCode == 83) {
+$(function() {
+    document.onkeydown = function (e) {
+        // ctrl/cmd + s
+        if (e.ctrlKey || e.metaKey) {
+            if (e.keyCode == 83) {
+                e.preventDefault()
+                e.stopPropagation()
+                saveDocument()
+            }
+        }
+
+        // tab
+        if (e.keyCode == 9) {
             e.preventDefault()
-            e.stopPropagation()
-            saveDocument()
+            typeInTextarea("	")
         }
     }
 
-    // tab
-    if (e.keyCode == 9) {
-        e.preventDefault()
-        typeInTextarea("	")
+    drop.ondragover = () => {
+        return false
     }
-}
 
-dropbox.ondragover = dropbox.ondragend = dropbox.ondragleave = () => {
-    return false
-}
+    drop.ondragend = () => {
+        return false
+    }
 
-dropbox.ondrop = (e) => {
-    e.preventDefault()
+    drop.ondragleave = () => {
+        return false
+    }
 
-    const file = e.dataTransfer.files[0]
-    const name = file.name
-    const type = file.type
-    const path = file.path
-    const size = file.size
+    drop.ondrop = (e) => {
+        e.preventDefault()
 
-    fs.readFile(file.path, "utf-8", (err, data) => {
-        if (err) throw err
-        setDocument(file, data)
-    })
+        const file = e.dataTransfer.files[0]
+        const name = file.name // File Name
+        const type = file.type // MIME type
+        const path = file.path // File Path
+        const size = file.size // File Size (in Bytes)
 
-    return false
-}
+        fs.readFile(file.path, "utf-8", (err, data) => {
+            if (err) throw err
+            setDocument(file, data)
+        })
+
+        return false
+    }
+})
